f601adeb2cdf67490f05c721939e20adf9691ed0
Sunil Shrestha <sunilshrestha2061@gmail.com>
2026-08-31 22:45:02 +0545

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
+// server.js
+
 require("dotenv").config();
 
-const app = require("./app");
+const express = require("express");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 5000;
+const authRoutes = require("./routes/auth");
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API is running",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
